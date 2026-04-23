11a4b5afd5cddc18af7447368203161eabab2d2b
habib-wahid <habib11wahid@gmail.com>
2026-04-23 13:17:42 +0600

    Open Api config changed to adopt jwt security

--- src/main/java/com/example/ticketing_app/config/OpenApiConfig.java
+++ src/main/java/com/example/ticketing_app/config/OpenApiConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
+
+	private static final String BEARER_SCHEME = "bearerAuth";
 
 	@Bean
 	public OpenAPI ticketingOpenApi() {
@@ -15,7 +19,12 @@ public class OpenApiConfig {
 				.info(new Info()
 						.title("Ticketing API")
 						.version("1.0")
-						.description("CRUD APIs for users, tickets, and SLA policies"));
+						.description("CRUD APIs for users, tickets, and SLA policies"))
+				.addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+				.schemaRequirement(BEARER_SCHEME, new SecurityScheme()
+						.name("Authorization")
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT"));
 	}
 }
-
